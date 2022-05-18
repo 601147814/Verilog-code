5c83c73ce72d2beb244df282eb50cb5ab63451cd
601147814 <31040168+601147814@users.noreply.github.com>
2022-05-18 10:51:51 +0800

    Create data_delay.v

---
+++ data_delay.v
@@ -0,0 +1,34 @@
+module data_delay#(
+		parameter DW 	= 8'd8		,
+		parameter N 	= 8'd8		
+	)(
+		input				clk		,
+		input				rst		,
+		input[DW-1:0]		din		,
+		output[DW-1:0]		dout	
+	);
+	
+	generate if( N == 0 )begin:No_Delay
+	
+		assign dout = din;
+		
+	end
+	else begin:Delay
+	
+	
+		reg[N*DW-1:0]data_reg;
+		
+		always@(posedge clk or posedge rst )begin
+			if( rst == 1'b1 )begin
+				data_reg <= {(N*DW){1'b1}};
+			end
+			else begin
+				data_reg <= {data_reg[0+:((N-1)*DW)],din};
+			end
+		end
+		
+		assign dout = data_reg[N*DW-1-:DW];
+	
+	end
+	
+	endmodule
