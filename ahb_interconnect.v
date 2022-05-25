6797927de53fcc8a9f37fa4643716417aeca52f8
601147814 <31040168+601147814@users.noreply.github.com>
2022-05-25 16:40:07 +0800

    Update ahb_interconnect.v

--- ahb_interconnect.v
+++ ahb_interconnect.v
@@ -43,8 +43,8 @@
 	initial begin 
 		if( AW%8 != 0 )begin $display("$s$d DW is suggested to be a multiple of 8.",`__FILE__,`__LINE__);end
 		if( DW%8 != 0 )begin $display("$s$d DW is suggested to be a multiple of 8.",`__FILE__,`__LINE__);end
-		if( M_NUM > 16 || M_NUM == 0)begin $display("$s$d M_NUM must be smaller than 16",`__FILE__,`__LINE__);end
-		if( S_NUM > 16 || S_NUM == 0)begin $display("$s$d M_NUM must be smaller than 16",`__FILE__,`__LINE__);end
+		if( M_NUM > 16 || M_NUM == 0)begin $display("$s$d M_NUM must be smaller than 16",`__FILE__,`__LINE__); $finished; end
+		if( S_NUM > 16 || S_NUM == 0)begin $display("$s$d M_NUM must be smaller than 16",`__FILE__,`__LINE__); $finished; end
 	end
 	
 	wire clk 	= HCLK		;
