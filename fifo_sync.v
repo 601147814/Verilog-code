dd9bebe30f2713069af8a3e7c69edfd1283a36b0
601147814 <31040168+601147814@users.noreply.github.com>
2022-07-22 17:45:56 +0800

    Update fifo_sync.v

--- fifo_sync.v
+++ fifo_sync.v
@@ -38,14 +38,14 @@
 		input				wr_en		,
 		input				rd_en		,
 		output	[DW-1:0]	dout		,
-		output	reg			empty		,
-		output	reg			alempty		,
-		output	reg			progempty	,	// when EMPTY_HOLD == 1, progempty is same with alempty
-		output	reg			full		,
-		output	reg			alfull		,
-		output	reg			progfull	,	// when FULL_HOLD == 1, progfull is same with alfull
-		output	reg[AW:0]	rd_space	,	// fifo space could be read
-		output  reg[AW:0]	wr_space		// fifo space could be written
+		output				empty		,
+		output				alempty		,
+		output				progempty	,	// when EMPTY_HOLD == 1, progempty is same with alempty
+		output				full		,
+		output				alfull		,
+		output				progfull	,	// when FULL_HOLD == 1, progfull is same with alfull
+		output	[AW:0]		rd_space	,	// fifo space could be read
+		output  [AW:0]		wr_space		// fifo space could be written
 	);
 	
 	initial begin
@@ -126,150 +126,162 @@
 	end
 	
 	assign dout <= (empty == 1'b1)? {DW{1'b0}} : mem[rd_p[AW-1:0]];
+		
+	assign rd_space == wr_p_temp - rd_p;
+	assign wr_space == rp_p_temp - wr_p - DEPTH;
+		
+	assign empty = ( wr_p_temp == rd_p )? 1'b1 : 1'b0;
+	assign full  = ( (wr_p[AW] != rd_p_temp[AW]) && (wr_p[0+:AW] == rd_p_temp[0+:AW]))? 1'b1 : 1'b0;
 	
-	always@( posedge rst or posedge clk )begin
-		if( rst == 1'b1 )begin
-			rd_space <= {(AW+1){1'b0}};
-		end
-		else if(( empty == 1'b0 ) && ( rd_en == 1'b1 ))begin
-			rd_space <= wr_p_temp - rd_p - 1'b1;
-		end
-		else begin
-			rd_space <= wr_p_temp - rd_p;
-		end
-	end	
+	assign alempty = ( rd_space <= 1'b1 )? 1'b1 : 1'b0;
+	assign alfull  = ( wr_space <= 1'b1 )? 1'b1 : 1'b0;
 	
-	always@( posedge rst or posedge clk )begin
-		if( rst == 1'b1 )begin
-			wr_space <= {1'b1,{AW{1'b0}}};
-		end
-		else if(( empty == 1'b0 ) && ( rd_en == 1'b1 ))begin
-			wr_space <= rp_p_temp - wr_p - DEPTH - 1'b1;
-		end
-		else begin
-			wr_space <= rp_p_temp - wr_p - DEPTH;
-		end
-	end
-	
-	always@( posedge rst or posedge clk )begin
-		if( rst == 1'b1 )begin
-			empty <= 1'b1;
-		end
-		else if( wr_p_temp == rd_p )begin
-			empty <= 1'b1;
-		end
-		else if( rd_space == 1'b1 )begin
-			if( rd_en == 1'b1 )begin
-				empty <= 1'b1;
-			end
-			else begin
-				empty <= 1'b0;
-			end
-		end
-		else begin
-			empty <= 1'b0;
-		end
-	end
-
-	always@( posedge rst or posedge clk )begin
-		if( rst == 1'b1 )begin
-			full <= 1'b0;
-		end
-		else if( (wr_p[AW] != rd_p_temp[AW]) && (wr_p[0+:AW] == rd_p_temp[0+:AW]))begin
-			full <= 1'b1;
-		end
-		else if( wr_space == 1'b1 )begin
-			if( wr_en == 1'b1 )begin
-				full <= 1'b1;
-			end
-			else begin
-				full <= 1'b0;
-			end
-		end
-		else begin
-			full <= 1'b0;
-		end
-	end		
-	
-	always@( posedge rst or posedge clk )begin
-		if( rst == 1'b1 )begin
-			alempty <= 1'b1;
-		end
-		else if( rd_space <= 2 )begin
-			alempty <= 1'b1;
-		end
-		else if( rd_space == 2'd2 )begin
-			if( rd_en == 1'b1 )begin
-				alempty <= 1'b1;
-			end
-			else begin
-				alempty <= 1'b0;
-			end
-		end
-		else begin
-			alempty <= 1'b0;
-		end
-	end
-
-	always@( posedge rst or posedge clk )begin
-		if( rst == 1'b1 )begin
-			progempty <= 1'b1;
-		end
-		else if( rd_space <= (EMPTY_HOLD + 1'b1) )begin
-			progempty <= 1'b1;
-		end
-		else if( rd_space == (EMPTY_HOLD + 1'b1) )begin
-			if( rd_en == 1'b1 )begin
-				progempty <= 1'b1;
-			end
-			else begin
-				progempty <= 1'b0;
-			end
-		end
-		else begin
-			progempty <= 1'b0;
-		end
-	end
-
-	always@( posedge rst or posedge clk )begin
-		if( rst == 1'b1 )begin
-			alfull <= 1'b1;
-		end
-		else if( wr_space < 2'd2 )begin
-			alfull <= 1'b1;
-		end
-		else if( wr_space == 2'd2 )begin
-			if( wr_en == 1'b1 )begin
-				alfull <= 1'b1;
-			end
-			else begin
-				alfull <= 1'b0;
-			end
-		end
-		else begin
-			alfull <= 1'b0;
-		end
-	end
-	
-	always@( posedge rst or posedge clk )begin
-		if( rst == 1'b1 )begin
-			progfull <= 1'b1;
-		end
-		else if( wr_space < (FULL_HOLD + 1'b1) )begin
-			progfull <= 1'b1;
-		end
-		else if( wr_space == (FULL_HOLD + 1'b1) )begin
-			if( wr_en == 1'b1 )begin
-				progfull <= 1'b1;
-			end
-			else begin
-				progfull <= 1'b0;
-			end
-		end
-		else begin
-			progfull <= 1'b0;
-		end
-	end
+	assign proempty = ( rd_space <= FULL_HOLD  )? 1'b1 : 1'b0;
+	assign profull  = ( wr_space <= EMPTY_HOLD )? 1'b1 : 1'b0;
+		
+//	always@( posedge rst or posedge clk )begin
+//		if( rst == 1'b1 )begin
+//			rd_space <= {(AW+1){1'b0}};
+//		end
+//		else if(( empty == 1'b0 ) && ( rd_en == 1'b1 ))begin
+//			rd_space <= wr_p_temp - rd_p - 1'b1;
+//		end
+//		else begin
+//			rd_space <= wr_p_temp - rd_p;
+//		end
+//	end	
+//	
+//	always@( posedge rst or posedge clk )begin
+//		if( rst == 1'b1 )begin
+//			wr_space <= {1'b1,{AW{1'b0}}};
+//		end
+//		else if(( empty == 1'b0 ) && ( rd_en == 1'b1 ))begin
+//			wr_space <= rp_p_temp - wr_p - DEPTH - 1'b1;
+//		end
+//		else begin
+//			wr_space <= rp_p_temp - wr_p - DEPTH;
+//		end
+//	end
+//	
+//	always@( posedge rst or posedge clk )begin
+//		if( rst == 1'b1 )begin
+//			empty <= 1'b1;
+//		end
+//		else if( wr_p_temp == rd_p )begin
+//			empty <= 1'b1;
+//		end
+//		else if( rd_space == 1'b1 )begin
+//			if( rd_en == 1'b1 )begin
+//				empty <= 1'b1;
+//			end
+//			else begin
+//				empty <= 1'b0;
+//			end
+//		end
+//		else begin
+//			empty <= 1'b0;
+//		end
+//	end
+//
+//	always@( posedge rst or posedge clk )begin
+//		if( rst == 1'b1 )begin
+//			full <= 1'b0;
+//		end
+//		else if( (wr_p[AW] != rd_p_temp[AW]) && (wr_p[0+:AW] == rd_p_temp[0+:AW]))begin
+//			full <= 1'b1;
+//		end
+//		else if( wr_space == 1'b1 )begin
+//			if( wr_en == 1'b1 )begin
+//				full <= 1'b1;
+//			end
+//			else begin
+//				full <= 1'b0;
+//			end
+//		end
+//		else begin
+//			full <= 1'b0;
+//		end
+//	end		
+//	
+//	always@( posedge rst or posedge clk )begin
+//		if( rst == 1'b1 )begin
+//			alempty <= 1'b1;
+//		end
+//		else if( rd_space <= 2 )begin
+//			alempty <= 1'b1;
+//		end
+//		else if( rd_space == 2'd2 )begin
+//			if( rd_en == 1'b1 )begin
+//				alempty <= 1'b1;
+//			end
+//			else begin
+//				alempty <= 1'b0;
+//			end
+//		end
+//		else begin
+//			alempty <= 1'b0;
+//		end
+//	end
+//
+//	always@( posedge rst or posedge clk )begin
+//		if( rst == 1'b1 )begin
+//			progempty <= 1'b1;
+//		end
+//		else if( rd_space <= (EMPTY_HOLD + 1'b1) )begin
+//			progempty <= 1'b1;
+//		end
+//		else if( rd_space == (EMPTY_HOLD + 1'b1) )begin
+//			if( rd_en == 1'b1 )begin
+//				progempty <= 1'b1;
+//			end
+//			else begin
+//				progempty <= 1'b0;
+//			end
+//		end
+//		else begin
+//			progempty <= 1'b0;
+//		end
+//	end
+//
+//	always@( posedge rst or posedge clk )begin
+//		if( rst == 1'b1 )begin
+//			alfull <= 1'b1;
+//		end
+//		else if( wr_space < 2'd2 )begin
+//			alfull <= 1'b1;
+//		end
+//		else if( wr_space == 2'd2 )begin
+//			if( wr_en == 1'b1 )begin
+//				alfull <= 1'b1;
+//			end
+//			else begin
+//				alfull <= 1'b0;
+//			end
+//		end
+//		else begin
+//			alfull <= 1'b0;
+//		end
+//	end
+//	
+//	always@( posedge rst or posedge clk )begin
+//		if( rst == 1'b1 )begin
+//			progfull <= 1'b1;
+//		end
+//		else if( wr_space < (FULL_HOLD + 1'b1) )begin
+//			progfull <= 1'b1;
+//		end
+//		else if( wr_space == (FULL_HOLD + 1'b1) )begin
+//			if( wr_en == 1'b1 )begin
+//				progfull <= 1'b1;
+//			end
+//			else begin
+//				progfull <= 1'b0;
+//			end
+//		end
+//		else begin
+//			progfull <= 1'b0;
+//		end
+//	end
 	
 
 	
