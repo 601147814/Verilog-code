804c8f6108fce0903fd5f4252222030e145808ba
601147814 <31040168+601147814@users.noreply.github.com>
2022-08-04 16:12:50 +0800

    Update Binary2Gray.v

--- Binary2Gray.v
+++ Binary2Gray.v
@@ -16,12 +16,14 @@
 		input  [DW-1:0] din	,
 		output [DW-1:0] dout
 	); 
+		
+	assign dout = din ^ {1'b0,din[DW-1:1]};
 	
-		assign dout[DW-1] = din[DW-1] ^ {1'b0,din[DW-1:1]};
-
-		integer i;	
-		generate for(i=0;i<=DW-1;i=i+1)	begin loop		
-			assign dout[i] = din[i+1] ^ din[i];
-		end
+	//	assign dout[DW-1] = din[DW-1] ^ {1'b0,din[DW-1:1]};
+	//
+	//	integer i;	
+	//	generate for(i=0;i<=DW-1;i=i+1)	begin loop		
+	//		assign dout[i] = din[i+1] ^ din[i];
+	//	end
 	
 	endmodule
