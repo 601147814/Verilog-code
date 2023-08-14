3684dc494aa2724aeef30c18d8b67f3b8690ba9d
601147814 <31040168+601147814@users.noreply.github.com>
2023-08-14 14:13:27 +0800

    Update data_delay.v

--- data_delay.v
+++ data_delay.v
@@ -1,56 +1,34 @@
-	///////////////////////////////////////////////////
-	// 
-	// sync_dff is for data register to filp folp in clock regin
-	//
-	//	sync_dff#(
-	//		.WIDTH	( 1		),
-	//		.DFF 	( 2		),
-	//		.LOGIC	( 2		)
-	//	)(
-	//		.rst	(		),
-	//		.clk	(		),
-	//		.din	(		),
-	//		.dout   (		)
-	//	)
-	//
-	//////////////////////////////////////////////////
-	
-	module sync_dff#(
-		parameter WIDTH = 1	,
-		parameter DFF 	= 2	,
-		parameter LOGIC = 2		//0: or; 1: and; 2: nothing
+module data_delay#(
+		parameter DW 	= 8'd8		,
+		parameter N 	= 8'd8		
 	)(
-		input 					rst		,
-		input 					clk		,
-		input	[WIDTH-1:0] 	din		,
-		output	[WIDTH-1:0]		dout
-	)
-	
-	reg[WIDTH*DFF-1:0] data_reg;
-	
-	always@(posedge clk or posedge rst)begin
-		if(rst == 1'b1)begin 
-			data_reg <= {(WIDTH*DFF)1'b0};
-		end
-		else begin	
-			data_reg <= {data_reg[0+:WIDTH*(DFF-1)],din};
-		end
+		input				clk		,
+		input				rst		,
+		input[DW-1:0]		din		,
+		output[DW-1:0]		dout	
+	);
+
+	generate if( N == 0 )begin:No_Delay
+
+		assign dout = din;
+
 	end
-	
-	generate if(WIDTH == 1)begin: bit1_op
-		generate if(LOGIC == 0)begin: or_op
-			assign	dout = |data_reg;
+	else begin:Delay
+
+
+		reg[N*DW-1:0]data_reg;
+
+		always@(posedge clk or posedge rst )begin
+			if( rst == 1'b1 )begin
+				data_reg <= {(N*DW){1'b1}};
+			end
+			else begin
+				data_reg <= {data_reg[0+:((N-1)*DW)],din};
+			end
 		end
-		else if (LOGIC == 1)begin: and_op
-			assign	dout = &data_reg;
-		end
-		else if(LOGIC == 2)begin: no_op
-			assign	dout = data_reg[WIDTH*(DFF-1)];
-		end
+
+		assign dout = data_reg[N*DW-1-:DW];
+
 	end
-	else if(WIDTH > 1)begin: 
-		assign dout = data_reg[WIDTH*DFF-1-:WIDTH];
-	end
-	endgenerate	
-	
+
 	endmodule
